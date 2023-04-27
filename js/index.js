c282a67874414f06a2d900ca19418b91a9c26448
vshivk <vshivkovavictoria@gmail.com>
2023-04-27 10:10:29 +0500

    fix: fixed sorting

--- js/index.js
+++ js/index.js
@@ -2,17 +2,22 @@ import {
     UI_ELEMENTS
 } from "./view.js";
 
-let data = JSON.parse(localStorage.getItem('data')) || [];
-let sortedByAlphabet = false;
+let DATA = JSON.parse(localStorage.getItem('data')) || [];
+let SORTED_BY_COLUMN = false;
+
+const setData = (NEW_DATA) => {
+    DATA = NEW_DATA;
+    return DATA;
+}
 
 const fetchPeople = async () => {
     UI_ELEMENTS.IS_LOADING.classList.remove('not-visible');
     UI_ELEMENTS.NO_DATA.classList.add('not-visible');
     try {
-        const response = await fetch('https://swapi.dev/api/people');
-        const json = await response.json();
-        data = json.results;
-        localStorage.setItem('data', JSON.stringify(data));
+        const RESPONSE = await fetch('https://swapi.dev/api/people');
+        const RESPONSE_JSON = await RESPONSE.json();
+        setData(RESPONSE_JSON.results);
+        localStorage.setItem('data', JSON.stringify(DATA));
         renderTable();
     } catch (error) {
         alert(error.message);
@@ -22,29 +27,33 @@ const fetchPeople = async () => {
 
 const clearTable = () => {
     localStorage.removeItem('data');
-    data = [];
+    setData([]);
     renderTable();
 };
 
 const removeRow = (index) => {
-    data.splice(index, 1);
-    localStorage.setItem('data', JSON.stringify(data));
+    DATA.splice(index, 1);
+    localStorage.setItem('data', JSON.stringify(DATA));
     renderTable();
 };
 
 const sortByColumn = (column) => {
-    sortedByAlphabet = !sortedByAlphabet;
-    if (sortedByAlphabet) {
-        data.sort((a, b) => a[column].localeCompare(b[column]));
-    } else {
-        data.sort((a, b) => b[column].localeCompare(a[column]));
-    }
-    localStorage.setItem('data', JSON.stringify(data));
+    SORTED_BY_COLUMN = !SORTED_BY_COLUMN;
+    const SORTED_DATA = DATA.slice().sort((a, b) => {
+        if (column === "height" || column === "mass") {
+            return SORTED_BY_COLUMN ? a[column] - b[column] : b[column] - a[column];
+        } else {
+            return SORTED_BY_COLUMN ? a[column].localeCompare(b[column]) : b[column].localeCompare(a[column]);
+        }
+    });
+    localStorage.setItem('data', JSON.stringify(SORTED_DATA));
+    setData(SORTED_DATA);
     renderTable();
 };
 
 const renderTable = () => {
-    if (data.length === 0) {
+    const IS_EMPTY = DATA.length === 0;
+    if (IS_EMPTY) {
         UI_ELEMENTS.NO_DATA.classList.remove('not-visible');
         UI_ELEMENTS.TABLE.classList.add('not-visible');
         UI_ELEMENTS.TABLE.innerHTML = '';
@@ -54,12 +63,12 @@ const renderTable = () => {
         UI_ELEMENTS.TABLE.innerHTML = `
             <tr class="table-thead">
                 <th onclick="sortByColumn('name')">Name</th>
-                <th>Height</th>
-                <th>Mass</th>
+                <th onclick="sortByColumn('height')">Height</th>
+                <th onclick="sortByColumn('mass')">Mass</th>
                 <th onclick="sortByColumn('hair_color')">Hair color</th>
                 <th onclick="sortByColumn('skin_color')">Skin color</th>
             </tr>
-            ${data.map((item, index) => `
+            ${DATA.map((item, index) => `
                 <tr>
                     <td>${item.name}</td>
                     <td>${item.height}</td>
@@ -74,9 +83,9 @@ const renderTable = () => {
         `;
     }
 };
-
+window.removeRow = removeRow;
+window.sortByColumn = sortByColumn;
 UI_ELEMENTS.LOAD_BTN.addEventListener('click', fetchPeople);
 UI_ELEMENTS.CLEAR_BTN.addEventListener('click', clearTable);
-
 renderTable();
 
